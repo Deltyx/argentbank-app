119bf74defd06d2146cacc9130c19ab2f09f6dda
Alexis <alexismorin.pro@gmail.com>
2022-10-17 16:14:03 +0200

    Added sign-in-form from design

--- src/views/sign_in/sign_in.js
+++ src/views/sign_in/sign_in.js
@@ -1,9 +1,11 @@
+import SignInForm from '../../components/sign_in_form/sign_in_form';
+
 import './sign_in.scss';
 
 export default function SignIn() {
     return ( 
         <main className="main bg-dark">
-
+            <SignInForm />
         </main>
     )
 }
